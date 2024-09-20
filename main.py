8bec259de31a9408af62195965016604a466c131
Fokku <shodusbus@gmail.com>
2024-09-20 13:12:51 +0800

    init

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-import sys
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from dash import Dash, html, dcc, Input, Output, State
+import requests
+import json
+
+app = Dash(__name__)
 
 def main():
-    print("test")
+    app.layout = [html.Div([html.H1("Job Data API")])]
+    app.run(debug=True)
 
 if __name__ == "__main__":
     main()
